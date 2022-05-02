6c57393ca6523a6642c1247312cffc68bde90b0c
Faeze-Farsi <103530748+Faeze-Farsi@users.noreply.github.com>
2022-05-02 22:12:14 +0430

    rand & bin2dec  function

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //var MyMessage = prompt(" What is your name ? ");
- //alert(" welcome  " + MyMessage );
+//alert(" welcome  " + MyMessage );
 
 
- // The variables
+//The variables
  let firstName = prompt("please enter your name : ");
- let age = prompt("please enter your age : ") ;
- let gender = prompt("please enter your gender : ") ;
- alert(" name :  " + firstName + " age : " + age + "  gender : " + gender);
+ let age1 = prompt("please enter your age : ") ;
+ let gender1 = prompt("please enter your gender : ") ;
+ alert(" name :  " + firstName + " age : " + age1 + "  gender : " + gender1);
 
  let score1= prompt(" please enter score 1 :");
  let score2= prompt(" please enter score 2 : ");
@@ -19,11 +19,33 @@
  
  //Data types
  let FirstName=prompt("please enter your first name : ");
- lastName=prompt("please enter your last name : ");
- age=prompt(" please enter your age : ");
- gender=prompt("please enter your gender : ");
+ let lastName=prompt("please enter your last name : ");
+ let age =prompt(" please enter your age : ");
+ let gender =prompt("please enter your gender : ");
  alert(`your first name is : ${FirstName} , your last name is : ${lastName} ,  your age is : ${age} , your gender is : ${gender} `);
  
  //Bonus:show info on the page
  let info =`your first name is : ${FirstName} , your last name is : ${lastName} ,  your age is : ${age} , your gender is : ${gender} `;
 document.getElementById("show").innerHTML= info;
+
+
+
+// Basic operators, maths
+
+//myrandom
+
+function random(min, max) {
+    randomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return randomNumber;
+}
+console.log(random(2,8)); 
+//bin2dec
+
+function bin2dec(string){
+     return parseInt(string,2);
+
+}
+let number ="1000";
+console.log(bin2dec(number));
+
+
