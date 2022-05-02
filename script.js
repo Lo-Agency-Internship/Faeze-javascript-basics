d5b46c5849b0c6765648d9c3520fb2e2fa73876a
Faeze-Farsi <103530748+Faeze-Farsi@users.noreply.github.com>
2022-05-02 17:30:04 +0430

    Data types & show info on the page

--- script.js
+++ script.js
@@ -17,4 +17,13 @@
  score2=score;
  alert(" score 1 is : "+score1+ "  score 2 is : " + score2 );
  
+ //Data types
+ let FirstName=prompt("please enter your first name : ");
+ lastName=prompt("please enter your last name : ");
+ age=prompt(" please enter your age : ");
+ gender=prompt("please enter your gender : ");
+ alert(`your first name is : ${FirstName} , your last name is : ${lastName} ,  your age is : ${age} , your gender is : ${gender} `);
  
+ //Bonus:show info on the page
+ let info =`your first name is : ${FirstName} , your last name is : ${lastName} ,  your age is : ${age} , your gender is : ${gender} `;
+document.getElementById("show").innerHTML= info;
